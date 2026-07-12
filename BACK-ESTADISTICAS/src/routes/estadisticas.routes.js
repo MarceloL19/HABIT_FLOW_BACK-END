942a5ccd651c6c40e02c842865bcbed11b5112a2
AlejandroOrtiz97 <20232089@aloe.ulima.edu.pe>
2026-07-12 08:54:43 -0500

    Delete BACK-ESTADISTICAS directory

--- BACK-ESTADISTICAS/src/routes/estadisticas.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { obtenerEstadisticasUsuario } from "../controllers/estadisticas.controller.js";
-
-const router = express.Router();
-
-router.get("/:idUsuario", obtenerEstadisticasUsuario);
-
-export default router;
